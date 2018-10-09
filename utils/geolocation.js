5581b28be6091937a4601a40281d9beb5bb91851
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2018-10-09 10:23:11 +0200

    Catch if android emulator and fail geolocation
    Encountered an issue where using expo on an Android emulator,
    fails to access the geoloaction. We throw a custom error, because the
    default error is difficult to debug

--- utils/geolocation.js
+++ utils/geolocation.js
@@ -2,8 +2,18 @@
  * Requests the current location of the user
  * @return {Promise} Resolves with location, or fails with error explaining why
  * */
+import { Platform } from 'react-native';
+import { Constants } from 'expo';
+
 export function getLocation() {
   return new Promise((rsv, rr) => {
+    // Geolocation fails on android emulators,
+    // with strange errors. As a fallback send a rejection
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      rr(new Error('Cannot fetch the geolocation on an Android Emulator. Try it on an device!'));
+      return;
+    }
+
     navigator.geolocation.getCurrentPosition(
       rsv,
       (err) => {
