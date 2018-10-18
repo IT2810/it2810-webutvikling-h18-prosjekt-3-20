a8f47ea9fd4a5d8d4743f2facd0d2307cdd88cdf
Ingrid Domben <ingrid.domben@gmail.com>
2018-10-18 12:54:08 +0200

    Add AgendaItem test

---
+++ components/__tests__/AgendaItem.js
@@ -0,0 +1,29 @@
+import 'react-native';
+import renderer from 'react-test-renderer';
+import React from 'react';
+import AgendaItem from '../AgendaItem';
+
+const mockedItems = [
+  {
+    name: 'Test',
+    time: '09:30:00',
+    location: 'Location',
+  },
+  {
+    name: 'Test',
+    location: 'Location',
+  },
+];
+
+describe('<AgendaItem/>', () => {
+  it('should render without failing', () => {
+    const tree = renderer.create(<AgendaItem item={mockedItems[0]}/>).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render without failing', () => {
+    const tree = renderer.create(<AgendaItem item={mockedItems[1]}/>).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
