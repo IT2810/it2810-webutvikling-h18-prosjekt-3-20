99e4d61c099d1640d2b32f3679fce4e455e0d376
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2018-10-18 08:29:05 +0200

    Closes #6: Add test coverage for gelocation handlers

--- utils/__tests__/geolocation.js
+++ utils/__tests__/geolocation.js
@@ -1,9 +1,49 @@
-import { getLocationOrNull } from '../geolocation';
+import { getLocation, getLocationOrNull } from '../geolocation';
+
+// Mock out Expo's permission and location api, for geolocation.js
+// This will unfortunately mean that we cannot test when Location does not work,
+// as it is set to approve before all tests are run
+jest.mock('expo', () => ({
+  Permissions: {
+    askAsync: jest.fn().mockImplementation(() => Promise.resolve({ status: 'granted' })),
+  },
+  Location: {
+    getCurrentPositionAsync: jest.fn().mockImplementation(() => Promise.resolve({
+      coords: {
+        accuracy: 65,
+        altitude: 48.78725814819336,
+        altitudeAccuracy: 10,
+        heading: -1,
+        latitude: 63.41534484650637,
+        longitude: 10.407017921963266,
+        speed: -1,
+      },
+      timestamp: 1539689316677.935,
+    })),
+  },
+}));
+
+describe('getLocation', () => {
+  it('should return a geolocation', async () => {
+    const location = await getLocation();
+
+    // We don't care about the actual values, but we want
+    // to verify the general form of the returned object
+    expect(typeof location).toBe('object');
+    expect('coords' in location).toBeTruthy();
+    expect('timestamp' in location).toBeTruthy();
+
+    expect('latitude' in location.coords).toBeTruthy();
+    expect('longitude' in location.coords).toBeTruthy();
+    expect('accuracy' in location.coords).toBeTruthy();
+  });
+});
 
 describe('getLocationOrNull', () => {
-  it('should return null on error', async () => {
-    const location = await getLocationOrNull();
+  it('should return the same as getLocation', async () => {
+    const expectedLocation = await getLocation();
+    const locationToTest = await getLocationOrNull();
 
-    expect(location).toBe(null);
+    expect(locationToTest).toEqual(expectedLocation);
   });
 });
